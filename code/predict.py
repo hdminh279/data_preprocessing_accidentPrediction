637a2d1bd27958ae0786b346e01cee7f51159438
Ho Duc Minh <hoducminh09@gmail.com>
2026-03-31 16:06:37 +0700

    update train_model increase accuracy

--- code/predict.py
+++ code/predict.py
@@ -12,20 +12,22 @@ spark = SparkSession.builder \
 saved_model = RandomForestClassificationModel.load("../model/random_forest_model")
 print("[SUCCESS] Đã load model thành công")
 
-    # "road_type_idx",
-    # "casualty_class_idx",
-    # "sex_of_casualty_idx",
-    # "light_conditions_idx",
-    # "weather_conditions_idx",
-    # "road_surface_conditions_idx",
-    # "urban_or_rural_area_idx",
-    # "time_bin_idx",
-    # "casualty_type_idx",
-    # "day_of_week"
-    # "age_of_casualty",
-    # "speed_limit"
+# Danh sách feature dùng cho mô hình:
+# - age_of_casualty
+# - speed_limit
+# - road_type_idx
+# - casualty_class_idx
+# - sex_of_casualty_idx
+# - light_conditions_idx
+# - weather_conditions_idx
+# - road_surface_conditions_idx
+# - urban_or_rural_area_idx
+# - time_bin_idx
+# - casualty_type_idx
+# - day_of_week
 
-sample_features = Vectors.dense([0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0])
+# sample_features = Vectors.dense([16, 60, 0, 0, 1, 0, 0, 1, 0, 0, 7, 1])
+sample_features = Vectors.dense([21,30,4,1,1,1,1,1,0,0,0,4])
 demo_df = spark.createDataFrame([(sample_features,)], ["features"])
 
 print("\n[INFO] Đang phân tích mức độ nghiêm trọng...")
--- code/train_model.py
+++ code/train_model.py
@@ -22,8 +22,10 @@ df_ml = spark.read \
     .option("RecursiveFileLookup", "true") \
     .parquet("../data/clean/optimize/")
 
-print("[INFO] Đang chia tập Train/Test bằng kỹ thuật Anti Join...")
+df_ml = df_ml.withColumn("smoothed_weight", F.sqrt(F.col("class_weight")))
 
+
+print("[INFO] Đang chia tập Train/Test bằng kỹ thuật Anti Join...")
 df_with_id = df_ml.withColumn("id", F.monotonically_increasing_id())
 
 ratio = {1: 0.8, 2: 0.8, 3: 0.8}
@@ -37,13 +39,14 @@ df_test = df_test.drop("id")
 df_train.persist(StorageLevel.MEMORY_AND_DISK)
 df_test.persist(StorageLevel.MEMORY_AND_DISK)
 
-print("[INFO] Đang khởi tạo mô hình Random Forest (Sử dụng class_weight)...")
+
+print("[INFO] Đang khởi tạo mô hình Random Forest...")
 rf = RandomForestClassifier(
     featuresCol="features",
     labelCol="casualty_severity",
-    weightCol="class_weight",
+    weightCol="smoothed_weight",
     numTrees=50,               
-    maxDepth=10,               
+    maxDepth=12,             
     maxBins=64,        
     seed=42
 )
@@ -66,26 +69,18 @@ evaluator_acc = MulticlassClassificationEvaluator(
 accuracy = evaluator_acc.evaluate(predictions)
 
 print("="*40)
-print(f"HIỆU SUẤT MÔ HÌNH RANDOM FOREST (FULL DATA + CLASS WEIGHT):")
+print(f"HIỆU SUẤT MÔ HÌNH RANDOM FOREST (FULL DATA):")
 print(f"- Độ chính xác tổng thể (Accuracy): {accuracy * 100:.2f}%")
 print(f"- Điểm F1-Score: {f1_score * 100:.2f}%")
 print("="*40)
 
-print("\n[DEMO] THỬ NGHIỆM DỰ ĐOÁN MỘT VỤ TAI NẠN MỚI")
-sample_features = Vectors.dense([1.5, 2.0, 1.0, 2.0, 1.0, 1.0, 2.0, 4.0, 0.0, 5.0, 2.0, 3.0])
 
-demo_df = spark.createDataFrame([(sample_features,)], ["features"])
-demo_prediction = rf_model.transform(demo_df)
+print("\n[INFO] ĐANG TẠO MA TRẬN NHẦM LẪN (CONFUSION MATRIX)...")
+print("-> Cột dọc (Trái): Nhãn thực tế ngoài đời")
+print("-> Cột ngang (Trên): Nhãn do máy tính dự đoán")
+confusion_matrix = predictions.crosstab("casualty_severity", "prediction")
+confusion_matrix.orderBy("casualty_severity_prediction").show()
 
-result = demo_prediction.select("prediction", "probability").collect()[0]
-predicted_label = int(result["prediction"])
-probabilities = result["probability"].toArray()
-
-print(f"Đầu vào (Features): {sample_features}")
-print(f"Mô hình dự đoán Mức độ nghiêm trọng là: NHÃN {predicted_label}")
-print(f"Xác suất máy tính nghĩ nó là Nhãn 1 (Tử vong): {probabilities[1]*100:.2f}%")
-print(f"Xác suất máy tính nghĩ nó là Nhãn 2 (Nặng): {probabilities[2]*100:.2f}%")
-print(f"Xác suất máy tính nghĩ nó là Nhãn 3 (Nhẹ): {probabilities[3]*100:.2f}%")
 
 print("\n[INFO] Đang lưu mô hình xuống ổ cứng...")
 rf_model.write().overwrite().save("../model/random_forest_model_full")
